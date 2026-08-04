1099e5b779e362e49ab1f0f0be359093b90378d3
Ayush <adayush1206@gmail.com>
2026-08-04 23:14:13 +0530

    Subset sum problems Completed

---
+++ Recursion Problems/Recursion-LC/SubsetSumII.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetSumII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ansList = new ArrayList<>();
+        findSubsets(0, nums, new ArrayList<>, ansList);
+        return ansList;
+    }
+    public void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
+        ansList.add(ds);
+        for (int i = ind; i < nums.length; i++) {
+            if (i != ind && nums[i] == nums[i-1]) continue;
+            ds.add(nums[i]);
+            findSubsets(ind+1, nums, ds, ansList);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
